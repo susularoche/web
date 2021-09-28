7669d8e3f6a50ebad132a5e1a004ebcb1403374d
Ben Walker <ben.walker.1212.dev@gmail.com>
2021-09-28 11:09:33 +0200

    draft two

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-/* =====================================================================================
-   Title
-   ===================================================================================== */
+// creates a footer element on every page
+const footer = document.createElement("footer"); // Create a <button> element
+footer.innerHTML = "S LAROCHE ©";
+const body = document.querySelector("body");
+body.appendChild(footer);
