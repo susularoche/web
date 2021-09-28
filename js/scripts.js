6b80f080b95e1ea5649c0f02e81f08a89436f1d1
Ben Walker <ben.walker.1212.dev@gmail.com>
2021-09-28 15:40:43 +0200

    third draft

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,9 @@ const footer = document.createElement("footer"); // Create a <button> element
 footer.innerHTML = "S LAROCHE ©";
 const body = document.querySelector("body");
 body.appendChild(footer);
+
+// const backBtn = document.createElement("a");
+// backBtn.innerHTML = "<";
+// backBtn.href = "index.html";
+// backBtn.classList.add("back-link");
+// body.prepend(backBtn);
